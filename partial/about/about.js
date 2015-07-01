660b7caf14149c30af03682d0e99933ac46dc62c
victorhqc <victorhqc@gmail.com>
2015-07-01 17:25:40 -0600

    About works again

--- partial/about/about.js
+++ partial/about/about.js
@@ -1,4 +1,163 @@
-angular.module('victorhqc').controller('AboutCtrl',function($scope){
+angular.module('victorhqc').controller('AboutCtrl',function($scope, supportsSVG, $timeout){
 
+    function getGravatarImg(email, size) {
+        var hash = md5(email);
+        var url = 'http://www.gravatar.com/avatar/' + hash;
+        url += '?s=' + size;
+        return url;
+    }
+
+    function render_profile_picture(){
+        var src = getGravatarImg('victorhqc@gmail.com', 350);
+        var img = document.getElementById('profile_pic');
+        img.src = src;
+    }
+
+    /**
+    * Detect SVG support
+    * Code thanks to @todd way to go!
+    * http://toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script/
+    */
+    function detect_svg_support() {
+        if (supportsSVG.check()) {
+            document.documentElement.className += ' svg';
+        } else {
+            document.documentElement.className += ' no-svg';
+            var imgs = document.getElementsByTagName('img');
+            var dotSVG = /.*\.svg$/;
+            for (var i = 0; i !== imgs.length; ++i) {
+                if(imgs[i].src.match(dotSVG)) {
+                    imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+                }
+            }
+        }
+    }
+
+    function onScroll(evt) {
+        if(!$scope.ticking){
+            $scope.ticking = true;
+            window.requestAnimFrame(updateElements());
+            $scope.lastScrollY = window.scrollY;
+        }
+    }
+
+    function visibleImgs() {
+        var visibles = [];
+        var invisibles = [];
+
+        var totalHeight = $scope.height - $scope.headerHeight;
+
+        var winscroll = $(window).scrollTop();
+        var relativeTop = winscroll + $scope.headerHeight;
+
+        var containers = document.getElementsByClassName('img-parallax');
+        for(var i = 0, len = containers.length; i < len; i++){
+            var container = containers[i];
+            var parent = container.parentNode.parentNode;
+
+            var upper = relativeTop - $(parent).offset().top + totalHeight ; // <----- Upper bound of image
+            var lower = upper - container.offsetHeight; // <----- Lower bound of image
+
+            var visible = false;
+            if(upper > 0 && lower < totalHeight){
+                visible = true;
+            }
+
+            container._upper = upper;
+            container._lower = lower;
+
+            if(visible === false){
+                invisibles.push(container);
+            }else{
+                visibles.push(container);
+            }
+        }
+
+        return {visibles:visibles, invisibles:invisibles};
+    }
+
+    window.requestAnimFrame = function(){
+        return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+    };
+
+    function updateElements() {
+        var imgs = visibleImgs();
+
+        for(var i = 0, len = imgs.invisibles.length; i < len; i++){
+            var aux = imgs.invisibles[i];
+            var img = aux.getElementsByTagName('img')[0];
+            img.style.visibility = 'hidden';
+            prefix(img.style, 'Transform', '');
+        }
+
+        for(var j = 0, len2 = imgs.visibles.length; j < len2; j++){
+            var aux2 = imgs.visibles[j];
+            var img2 = aux2.getElementsByTagName('img')[0];
+
+            img2.style.visibility =  'visible';
+
+            var relScroll = aux2._lower;
+
+            var imgH = aux2.offsetHeight;
+            var op2 = -100 + Math.round( -1 * relScroll * 0.15 - (imgH * 0.05));
+
+            prefix(img2.style, "Transform", "translate3d(0," + op2 + "px, 0)");
+        }
+
+        $scope.ticking = false;
+
+    }
+
+    function prefix(obj, prop, value) {
+        var prefs = ['webkit', 'Moz', 'o', 'ms'];
+        for (var pref in prefs) {
+            obj[prefs[pref] + prop] = value;
+        }
+    }
+
+    $scope.features = [
+        {
+            "title":"feature_1",
+            "message":"featurem_1",
+            "icon":"ultrabook.png"
+        },
+        {
+            "title":"feature_2",
+            "message":"featurem_2",
+            "icon":"type.png"
+        },
+        {
+            "title":"feature_3",
+            "message":"featurem_3",
+            "icon":"news.png"
+        },
+        {
+            "title":"feature_4",
+            "message":"featurem_4",
+            "icon":"mug.png"
+        },
+        {
+            "title":"feature_5",
+            "message":"featurem_5",
+            "icon":"gameboy.png"
+        }
+    ];
+
+    $timeout(function() {
+        $scope.height = $(window).height();
+        $scope.headerHeight = document.getElementById('header').clientHeight;
+
+        window.addEventListener('scroll', onScroll, false);
+
+        updateElements();
+        render_profile_picture();
+    }, 0);
 
 });
--- translations/translations.js
+++ translations/translations.js
@@ -17,6 +17,9 @@ angular.module('translations').config(function($translateProvider) {
         $translateProvider.preferredLanguage('es_MX');
         break;
     }
+
+    $translateProvider.useSanitizeValueStrategy('escaped');
+
     $translateProvider.translations('es_MX', {
         home: 'Inicio',
         about: 'Acerca de',
@@ -35,7 +38,49 @@ angular.module('translations').config(function($translateProvider) {
         legal_3: "Los logos son propiedad de",
         legal_4: "Github",
         legal_5: "y",
-        legal_6: "W3C"
+        legal_6: "W3C",
+        jum_title: "Desarrollador Senior en Backend & Frontend",
+        jum_description_1: "Soy un apasionado programador con la inalcanzable misión de lograr el código perfecto. He participado en numerosos proyectos de desarrollo, desde páginas de internet hasta avanzadas aplicaciones web.",
+        jum_description_2: "Estudié en la Universidad La Salle la Ingeniería en Cibernética ",
+        jum_desc_help_2: " - generación 2006-2011",
+        jum_description_2_1: " en Chihuahua, Chih. México. Sin embargo mi especialidad está en la programación Web, comencé a hacer sitios web en 2008, con experiencia profesional desde el 2010.",
+        jum_description_3: "Soy co fundador de ",
+        jum_desc_help_3: "uVicate",
+        jum_description_3_1: " una empresa dedicada al desarrollo de software en Internet, desempeñando el papel de director en el área de desarrollo.",
+        technical_knowledge: "Conocimiento técnico",
+        kn_title_1: "Diseño de bases de datos.",
+        kn_sub_1: "Con MySQL, SQLite.",
+        kb_main_1: "Desde realizar el modelo entidad-relación, diseñar el modelo relacional, escribir el código de sintáxis para crearla, cumpliendo con las reglas de normalización claro está. Hasta realizar consultas, funciones almacenadas, etc. y código avanzado de SQL.",
+        kn_title_2: "Programación Server side ",
+        kn_sub_2: "Utilizando PHP",
+        kb_main_2: "Tengo experiencia con la programación orientada a objetos en este lenguaje. He realizado una gran cantidad de librerías y API's en diferentes proyectos y sistemas.",
+        kn_title_3: "Programación Client side ",
+        kn_sub_3: "Utilizando JavaScript, HTML y CSS",
+        kn_main_3: "Utilizando los patrones de diseño más modernos el máximo rendimiento en los desarrollos. Además de tener experiencia con las tecnologías de HTML5 como IndexedDB y Web Sockets. Integración de servicios de terceros como Google, Dropbox, Facebook, etc.",
+        kn_title_4: "Códigos elegantes",
+        kn_sub_4: " y abiertos",
+        kn_main_4: "El código debe ser fácil de leer, mantener y expander. He desarrollado algunas APIs que se utilizán por diferentes desarrolladores en el mundo, ¡Revisa mis repositorios de código abierto en ",
+        kn_main_4_1: "Github",
+        kn_main_4_2: "! Si eres un programador como yo, me gustaría conocer tu trabajo también.",
+        kn_title_5: "Mi no tan especialidad ",
+        kn_sub_5: "Programación con Python",
+        kn_main_5: "En el 2012 Escribí una aplicación híbrida escrita en Python y tecnologías Web. Para Linux, específicamente para la distribución de Ubuntu. Y aunque nunca la publiqué, me inspiró para realizar el API \"WebApp\" y mi proyecto \"ExpMan\".",
+        contact_me: "Contáctame",
+        contact_phone: "Teléfono",
+        contact_email: "Correo electrónico",
+        about_social: "Redes sociales",
+        about_location: "Ubicación",
+        curiosities: "Datos curiosos",
+        feature_1: "8 Horas de programación",
+        featurem_1: "Diarias",
+        feature_2: "+10,000 Líneas de código",
+        featurem_2: "Mensuales",
+        feature_3: "2 Horas leyendo",
+        featurem_3:"Por día",
+        feature_4: "10 tazas de café",
+        featurem_4: "Por semana",
+        feature_5: "Dos partidas de League of legends",
+        featurem_5: "Al día"
     });
 
     $translateProvider.translations('en_US', {
